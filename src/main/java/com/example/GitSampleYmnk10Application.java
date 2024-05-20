34468663c1dc9629547487c72d9f63a85ccda297
ymnk10 <kseisaimin10@gmail.com>
2024-05-20 11:37:13 +0900

    新規にプロジェクトを作成

---
+++ src/main/java/com/example/GitSampleYmnk10Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSampleYmnk10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSampleYmnk10Application.class, args);
+	}
+
+}
